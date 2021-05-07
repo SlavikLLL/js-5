5d2cae167343bd03330e37e5def15aacfcd77da6
SlavikLLL <listopads091@gmail.com>
2021-05-07 14:46:17 +0300

    Initial commit

---
+++ js-5.js
@@ -0,0 +1,19 @@
+
+
+let space='';
+let NumberOfFloors= +prompt("Укажите кол-во ярусов");
+if(!isNaN(NumberOfFloors)){
+    for(let i=1;i<=NumberOfFloors;i++){
+        
+        for( let height=i;height<=NumberOfFloors-1;height++){
+            space += ' '; 
+        }
+        for( let NumberOfElements=i;NumberOfElements<=2*i-1;NumberOfElements++){
+            space += '*'; 
+        }
+        
+        
+     space +='\n'
+    }
+    console.log(space);
+}
