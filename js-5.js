427fe2a7dd5b21f50e8ae958cd6ab97e0cf3fa55
SlavikLLL <listopads091@gmail.com>
2021-05-12 18:21:14 +0300

    Update js-5.js

--- js-5.js
+++ js-5.js
@@ -5,11 +5,11 @@ let NumberOfFloors= +prompt("Укажите кол-во ярусов");
 if(!isNaN(NumberOfFloors)){
     for(let i=0;i<=NumberOfFloors;i++){
         
-        for( let height=0;height<=NumberOfFloors-1;height++){
+        for( let height=0;height<=NumberOfFloors-i;height++){
             space += ' '; 
         }
         for( let NumberOfElements=0;NumberOfElements<=2*i;NumberOfElements++){
-            space += '* '; 
+            space += '*'; 
         }
         
         
